2f49788288bf1d4efebee15a23f0827b654542cf
Misha Grebennikov <grebennikovmisha@gmail.com>
2021-12-15 20:07:31 +0200

    center, scientific, expert-and-research, link-list

--- dist/js/main.js
+++ dist/js/main.js
@@ -442,6 +442,66 @@ $(document).ready(function () {
     expansionSectionGroup();
     // filterSearch();
 
+    // Section Menu
+
+    // function sectionMenu() {
+    //     let $appScrollView = $('.app-scroll-view');
+    //     let sectionMenu = $('.section-menu');
+    //     let $sectionMenuItem = $sectionMenu.find('a');
+    //     $('a').click(function(){
+    //         var topOffset = 100;
+    //         $appScrollView.animate({
+    //         scrollTop: $($(this).attr('href')).offset().top-topOffset
+    //             }, 1500);
+    //         });
+    // }
+
+    var lastId;
+    var topMenu = $("#section-menu");
+    let $appScrollView = $('.app-scroll-view');
+    var topMenuHeight = topMenu.outerHeight(); //цифра это расстояние от верхушки меню до нужной секции(можно менять)
+    var menuItems = topMenu.find("a");
+        // Anchors corresponding to menu items
+    var scrollItems = menuItems.map(function(){
+        var item = $($(this).attr("href"));
+        if (item.length) { return item; }
+        });
+
+    // Bind click handler to menu items
+    // so we can get a fancy scroll animation
+    menuItems.click(function(e){
+    var href = $(this).attr("href"),
+        offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+11;//цифру можно менять или убрать..
+        $appScrollView.stop().animate({ 
+        scrollTop: offsetTop
+    }, 300);
+    e.preventDefault();
+    });
+
+    // Bind to scroll
+    $($appScrollView).scroll(function(){
+    // Get container scroll position
+    var fromTop = $(this).scrollTop()+topMenuHeight;
+    
+    // Get id of current scroll item
+    var cur = scrollItems.map(function(){
+        if ($(this).offset().top < fromTop)
+        return this;
+    });
+    // Get the id of the current element
+    cur = cur[cur.length-1];
+    var id = cur && cur.length ? cur[0].id : "";
+    
+    if (lastId !== id) {
+        lastId = id;
+        // Set/remove active class
+        menuItems
+            .parent().removeClass("active")
+            .end().filter("[href='#"+id+"']").parent().addClass("active");
+    }                   
+    });
+        
+
     // $($appScrollView).scroll(function () {
     //     var scrolled = $($appScrollView).scrollTop();
     //     let scrollDistance = 200;
